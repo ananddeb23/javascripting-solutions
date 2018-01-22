c6c9471062c478bfb9336468d178be96bc39b3c9
anand <ananddeb23@gmail.com>
2018-01-22 21:57:39 +0530

    javascripting excercises completed

---
+++ function.js
@@ -0,0 +1,6 @@
+// console.log(banana);
+console.log(eat(banana = 'test'));
+function eat(food){
+  return food + ' tasted really good.';
+}
+console.log(banana = 'banana');
---
+++ objects.js
@@ -0,0 +1,10 @@
+var foodPreferences = {
+pizza: 'yum',
+salad: 'gross'
+};
+var pizza = {
+toppings : ['cheese', 'sauce', 'pepperoni'],
+crust : 'deep dish',
+serves : 2
+};
+console.log(pizza);
